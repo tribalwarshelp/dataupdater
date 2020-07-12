5c069c919f36b462299be661b77cb6ce2f0c4812
Kichiyaki <xyztojajestem@gmail.com>
2020-07-12 11:52:11 +0200

    fix bug with saving best rank (lower rank should be better)

--- cron/sql_statements.go
+++ cron/sql_statements.go
@@ -111,7 +111,7 @@ const (
 				NEW.most_villages = NEW.total_villages;
 				NEW.most_villages_at = now();
 			END IF;
-			IF NEW.best_rank IS null OR NEW.rank > NEW.best_rank THEN
+			IF NEW.best_rank IS null OR NEW.rank < NEW.best_rank THEN
 				NEW.best_rank = NEW.rank;
 				NEW.best_rank_at = now();
 			END IF;
@@ -133,7 +133,7 @@ const (
 				NEW.most_villages = NEW.total_villages;
 				NEW.most_villages_at = now();
 			END IF;
-			IF NEW.best_rank IS null OR NEW.rank > OLD.best_rank THEN
+			IF NEW.best_rank IS null OR NEW.rank < OLD.best_rank THEN
 				NEW.best_rank = NEW.rank;
 				NEW.best_rank_at = now();
 			END IF;
